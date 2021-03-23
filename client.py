d70182dafaeaeef3966af79002bfd42675c4a9d5
LaKeR <yayaya.tytytyty@mail.ru>
2021-03-23 20:48:52 +0300

    1.4

--- client.py
+++ client.py
@@ -1,5 +1,3 @@
-"""---Имитация клиента---"""
-
 import sys
 import zmq
 import datetime
@@ -11,24 +9,7 @@ def main():
     zmq_socket.bind("tcp://127.0.0.1:5557")
     do = sys.argv[1]
     but = sys.argv[2]
+    print(do, but)
     zmq_socket.send_pyobj((do, but))
-
-
-"""# Connection to local server
-    ctx = zmq.Context()
-    socket = ctx.socket(zmq.PUB)
-    try:
-        socket.bind("tcp://*:1337") # instead of '1337' you can put any port
-        print('Connected to tcp://*:1337')
-    except:
-        print('Connection failed')
-
-# Button transmitter
-    #while True:
-        #msg = str(input('input command: '))
-    do = b"test"
-    but = 'tab'
-    socket.send(do) # this string send msg object on server
-    print('Message sent at:', datetime.datetime.now())"""
 
 main()
--- main.py
+++ main.py
@@ -2,46 +2,22 @@
 
 import keyboard
 import zmq
+import time
 
 def main():
     context = zmq.Context()
     consumer_receiver = context.socket(zmq.PULL)
     consumer_receiver.connect('tcp://127.0.0.1:5557')
     while True:
-        work = consumer_receiver.recv_pyobj()
-        print(work[0], work[1])
-
-""" Connection to local server
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.setsockopt_string(zmq.SUBSCRIBE, "")
-    try:
-        socket.connect("tcp://localhost:1337") # instead of '1337' you can put any port
-        print('Connected to tcp://localhost:1337')
-    except:
-        print('Connection failed')
-
-# Requests handler
-    while True:
-        msg = socket.recv_pyobj() # listen last message from server
-        print(msg)
+        msg = consumer_receiver.recv_pyobj()
         if len(msg) < 2 or len(msg) > 2:
             pass
         elif msg[0] == 'press':
-            try:
-                keyboard.press(msg[1])
-                print('pressed')
-            except:
-                print('Unexpected button')
+            keyboard.press(msg[1])
         elif msg[0] == 'release':
-            try:
-                keyboard.release(msg[1])
-                print('released')
-            except:
-                print('Key', '[' + msg[1] + ']', 'was not pressed')
+            keyboard.release(msg[1])
         else:
-            print(msg[1])"""
-
+            pass
 
 if __name__ == '__main__':
     main()
